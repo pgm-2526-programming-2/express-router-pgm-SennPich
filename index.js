1a536f6215aad25620afe6b30d6ed4b50a4677d2
pgm-SennPich <sennpich@student.arteveldehs.be>
2025-12-15 15:57:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get("/students", studentsController.getStudents);
-app.get("/students/:id", studentsController.getStudentById);
-app.put("/students/:id", studentsController.updateStudent);
-app.post("/students", studentsController.addStudent);
-app.delete("/students/:id", studentsController.removeStudent);
 
 app.get("/students", studentsRoutes);
 
