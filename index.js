a7461b9c432b8c87523f32ddcb58ff8867ef3023
pgm-SennPich <sennpich@student.arteveldehs.be>
2025-12-15 15:57:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const studentsController = require("./controllers/students.controller");
+const studentsRoutes = require("./models/studentsRouter.js");
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,8 @@ app.get("/students/:id", studentsController.getStudentById);
 app.put("/students/:id", studentsController.updateStudent);
 app.post("/students", studentsController.addStudent);
 app.delete("/students/:id", studentsController.removeStudent);
+
+app.get("/students", studentsRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
