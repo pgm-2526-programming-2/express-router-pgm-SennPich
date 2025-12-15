b69c8b657d40ba56251ef1b00531e84f7b879ee6
pgm-SennPich <sennpich@student.arteveldehs.be>
2025-12-15 15:58:47 +0100

    Update studentsRouter.js

--- routers/studentsRouter.js
+++ routers/studentsRouter.js
@@ -7,3 +7,5 @@ router.get("/students/:id", studentsController.getStudentById);
 router.put("/students/:id", studentsController.updateStudent);
 router.post("/students", studentsController.addStudent);
 router.delete("/students/:id", studentsController.removeStudent);
+
+module.exports = router;
