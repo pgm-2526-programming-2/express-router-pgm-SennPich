0965cb22d3771ff3a48ff0d183332e601608bd24
pgm-SennPich <sennpich@student.arteveldehs.be>
2025-12-15 15:48:21 +0100

    Update studentsRouter.js

--- routers/studentsRouter.js
+++ routers/studentsRouter.js
@@ -1,2 +1,6 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const paths = require("path");
+
+const students = require(paths.join(__dirname, "..", "controllers", "students.controller.js");
+
